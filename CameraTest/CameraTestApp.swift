2199b763de4f345ad3ff7a3f86d9ec9c80b864ec
RTsta <34354048+RTsta@users.noreply.github.com>
2022-09-29 14:55:14 +0200

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Arthur Nácar on 29.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
